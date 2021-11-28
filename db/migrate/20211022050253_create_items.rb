3a55cf8ac468fb713723efbd0b3b5a0d95e97df0
Monica Hung <monica.hung@mycase.com>
2021-11-27 18:27:34 -0800

    Migrate

--- db/migrate/20211022050253_create_items.rb
+++ db/migrate/20211022050253_create_items.rb
@@ -1,8 +1,8 @@
 class CreateItems < ActiveRecord::Migration[6.1]
   def change
     create_table :items do |t|
-      t.string :name
-      t.string :count
+      t.string :name, null: false
+      t.integer :count, null: false, default: 0
 
       t.timestamps
     end
